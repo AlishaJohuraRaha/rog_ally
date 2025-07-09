420d0487bc21fea9f39135f1e8a3fe153ccdf403
jaedalzaber <jaedalzaber@gmail.com>
2025-07-09 23:14:47 +0600

    .

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -12,24 +12,24 @@ export const dynamic = 'force-static'
 export const revalidate = 600
 
 export default async function Page() {
-  const payload = await getPayload({ config: configPromise })
+  // const payload = await getPayload({ config: configPromise })
 
-  const posts = await payload.find({
-    collection: 'posts',
-    depth: 1,
-    limit: 12,
-    overrideAccess: true,
-    select: {
-      title: true,
-      slug: true,
-      categories: true,
-      meta: true,
-    },
-  })
+  // const posts = await payload.find({
+  //   collection: 'posts',
+  //   depth: 1,
+  //   limit: 12,
+  //   overrideAccess: false,
+  //   select: {
+  //     title: true,
+  //     slug: true,
+  //     categories: true,
+  //     meta: true,
+  //   },
+  // })
 
   return (
     <div className="pt-24 pb-24">
-      <PageClient />
+      {/* <PageClient />
       <div className="container mb-16">
         <div className="prose dark:prose-invert max-w-none">
           <h1>Posts</h1>
@@ -51,7 +51,7 @@ export default async function Page() {
         {posts.totalPages > 1 && posts.page && (
           <Pagination page={posts.page} totalPages={posts.totalPages} />
         )}
-      </div>
+      </div> */}
     </div>
   )
 }
@@ -59,5 +59,29 @@ export default async function Page() {
 export function generateMetadata(): Metadata {
   return {
     title: `Payload Website Template Posts`,
+  }
+}
+
+export async function getStaticProps() {
+  const payload = await getPayload({ config: configPromise })
+   const posts = await payload.find({
+    collection: 'posts',
+    depth: 1,
+    limit: 12,
+    overrideAccess: false,
+    select: {
+      title: true,
+      slug: true,
+      categories: true,
+      meta: true,
+    },
+  })
+  if (!posts) {
+    return {
+      notFound: true,
+    }
+  }
+  return {
+    props: { posts },
   }
 }
