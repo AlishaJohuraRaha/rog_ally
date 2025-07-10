6ffb351526ce6a1db5cf7ca4c4dd1877e3ff1108
jaedalzaber <jaedalzaber@gmail.com>
2025-07-10 10:06:16 +0600

    .

--- next.config.js
+++ next.config.js
@@ -23,6 +23,11 @@ const nextConfig = {
   reactStrictMode: true,
   redirects,
   typescript: { ignoreBuildErrors: true },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
